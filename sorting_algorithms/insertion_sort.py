a8602b7bac7f01ceb14d72e0b522c41c6fec3148
aditi <aditi.nitttr@gmail.com>
2020-02-23 12:22:16 +0530

    insertion sort

--- sorting_algorithms/insertion_sort.py
+++ sorting_algorithms/insertion_sort.py
@@ -1,11 +1,17 @@
 A = [5, 2, 4, 6, 1, 3]
 
-for i in range(1, len(A)):
-    key = A[i]
-    j = i-1
-    while j >= 0 and A[j] > key:
-        A[j+1] = A[j]
-        j -= 1
-    A[j+1] = key
+def insertion_sort(A):
+    for i in range(1, len(A)):
+        key = A[i]
+        index = i-1
+        while index >= 0 and A[index] > key:
+            # Shift current index value to next index
+            A[index+1] = A[index]
+            # decrement index by -1
+            index -= 1
+        A[index+1] = key
+    return A
 
-print("Sorted array", A)
+
+sorted_array = insertion_sort(A)
+print("Sorted array", sorted_array)
